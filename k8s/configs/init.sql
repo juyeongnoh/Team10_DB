1e4f0c31d62b4b82199a924c0241ec8e165b0a58
김명지 <audwl03071@gmail.com>
2023-10-30 16:34:33 +0900

    fix: init.sql 수정

--- k8s/configs/init.sql
+++ k8s/configs/init.sql
@@ -23,19 +23,20 @@ FLUSH PRIVILEGES;
 USE `krampoline`;
 
 # db 예시
-# DROP TABLE IF EXISTS `sample_data`;
-# CREATE TABLE `sample_data`
-# (
-#     `id`     int(11)      NOT NULL AUTO_INCREMENT,
-#     `detail` varchar(100) NOT NULL,
-#     PRIMARY KEY (`id`)
-# ) ENGINE = InnoDB
-#   AUTO_INCREMENT = 3
-#   DEFAULT CHARSET = utf8mb4
-#   COLLATE = utf8mb4_general_ci;
-#
-# INSERT INTO sample_data (`id`, `detail`)
-# VALUES ('1', 'Hello DKOS!');
+DROP TABLE IF EXISTS `sample_data`;
+CREATE TABLE `sample_data`
+(
+    `id`     int(11)      NOT NULL AUTO_INCREMENT,
+    `detail` varchar(100) NOT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE = InnoDB
+  AUTO_INCREMENT = 3
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_general_ci;
+
+INSERT INTO sample_data (`id`, `detail`)
+VALUES ('1', 'Hi DKOS!');
+
 
 
 DROP TABLE IF EXISTS `bay`;
